dfeef024572ff10ed354d5513c8a040a66e6c4b9
c0ttntail <defordalex@gmail.com>
2024-07-24 15:57:45 -0700

    added smooth snapping

--- rumblechart/chart.js
+++ rumblechart/chart.js
@@ -34,7 +34,7 @@ var mouseS
 function mouseDetect(event) {
     var mouseC = event.buttons > 0;
     mouseV = Math.min(5, Math.sqrt((size*.5 - event.offsetX)**2 + (size*.5 - event.offsetY)**2) /30.6);
-    mouseV = mouseV + Math.abs(Math.sin(mouseV*Math.PI + Math.PI*.5))**.5 * ((Math.round(mouseV * 5) * .2) - mouseV)
+    mouseV = mouseV + (Math.abs(Math.sin(2*mouseV*Math.PI+Math.PI*0.5))**0.5 * (Math.round(Math.abs(mouseV-0.25 % -1)) * 0.25 + 0.5)) * ((Math.round(mouseV * 2) * 0.5) - mouseV);
     var mouseD = 1 + Math.round(.016667*Math.abs(90+ ((180/Math.PI) * Math.atan((event.offsetY-size*.5)/(event.offsetX-size*.5)) + 180*(event.offsetX < size*.5)))) % 6;
     if (mouseC == 0) {if (mouseS != 0) {mouseS = 0}} else {if (mouseS == 0) {mouseS = mouseD}};
     if (mouseS != 0) {stats[mouseS - 1] = mouseV};
