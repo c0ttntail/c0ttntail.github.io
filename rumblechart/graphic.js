7695033a37c8a98dabda598cff51d080b3976947
c0ttntail <defordalex@gmail.com>
2024-07-25 01:05:08 -0700

    shadow comp, layering playstyles

--- rumblechart/graphic.js
+++ rumblechart/graphic.js
@@ -1,36 +1,44 @@
 var baseimg = document.getElementById("base");
 var graphimg = document.getElementById("graph");
 var labelimg = document.getElementById("label");
-var div = document.getElementById("div");
+var contain = document.getElementById("contain");
+var add = document.getElementById("add");
+var dump = document.getElementById("dump");
+var sCompC = document.getElementById("sCompC");
 var c = document.getElementById("chart");
 var chart = c.getContext("2d");
 var size = 480;
 var stats = [3, 3, 3, 3, 3, 3]; // Mobility, Aerial, Spawn Pace, Range Biase, Reactivity, Creativity
 var polyHue = 180
 var hexSat = 0
+var sComp = false
 var show = [true, true, true, true]
+var extras = []
 chart.lineWidth = size/60;
+chart.shadowBlur = 15
+chart.shadowOffsetX = 0;
+chart.shadowOffsetY = 0;
 
 function drawHex() {
     for (let p = 1; p < 6; p++) {
-    chart.beginPath();
-    chart.moveTo((size*.5), (size*.5 + (6 - p)*size*.06375));
-    chart.lineTo((size*.5 - (6 - p)*size*0.055209118), (size*.5 + (6 - p)*size*0.031875));
-    chart.lineTo((size*.5 - (6 - p)*size*0.055209118), (size*.5 - (6 - p)*size*0.031875));
-    chart.lineTo((size*.5), (size*.5 - (6 - p)*size*.06375))
-    chart.lineTo((size*.5 + (6 - p)*size*0.055209118), (size*.5 - (6 - p)*size*0.031875))
-    chart.lineTo((size*.5 + (6 - p)*size*0.055209118), (size*.5 + (6 - p)*size*0.031875))
-    chart.closePath();
-    chart.fillStyle = "hsl("+polyHue+" "+hexSat+" "+(33 - p*5)+")";
-    chart.fill(); };
-    if (show[1] == true) {chart.drawImage(graphimg, 0, 0, size, size);}; };
+        chart.beginPath();
+        chart.moveTo((size*.5), (size*.5 + (6 - p)*size*.06375));
+        chart.lineTo((size*.5 - (6 - p)*size*0.055209118), (size*.5 + (6 - p)*size*0.031875));
+        chart.lineTo((size*.5 - (6 - p)*size*0.055209118), (size*.5 - (6 - p)*size*0.031875));
+        chart.lineTo((size*.5), (size*.5 - (6 - p)*size*.06375))
+        chart.lineTo((size*.5 + (6 - p)*size*0.055209118), (size*.5 - (6 - p)*size*0.031875))
+        chart.lineTo((size*.5 + (6 - p)*size*0.055209118), (size*.5 + (6 - p)*size*0.031875))
+        chart.closePath();
+        chart.fillStyle = "hsl("+polyHue+" "+hexSat+" "+(33 - p*5)+")";
+        chart.fill();
+        chart.shadowBlur = 15 * sComp };
+    chart.shadowBlur = 15; };
 function drawStats() {
     chart.clearRect(0,0, size, size);
-    if (hexSat == 0 && show[0] == true && show[1] == true && show[2] == true) {chart.drawImage(baseimg, 0, 0, size, size);} 
-    else { 
-        if (show[0] == true) {drawHex();}
-        if (show[1] == true) {chart.drawImage(graphimg, 0, 0, size, size);};
-        if (show[2] == true) {chart.drawImage(labelimg, 0, 0, size, size);}; };
+    chart.shadowColor = "hsl(0 0 0)";
+    if (show[0] == true) {drawHex();}
+    if (show[1] == true) {chart.drawImage(graphimg, 0, 0, size, size);};
+    if (show[2] == true) {chart.drawImage(labelimg, 0, 0, size, size);};
     if (show[3] == true) {
         chart.beginPath();
         chart.moveTo((size*.5), (size*.5 + stats[3]*size*.06375));
@@ -40,10 +48,25 @@ function drawStats() {
         chart.lineTo((size*.5 + stats[1]*size*0.055209118), (size*.5 - stats[1]*size*0.031875))
         chart.lineTo((size*.5 + stats[2]*size*0.055209118), (size*.5 + stats[2]*size*0.031875))
         chart.closePath();
+        chart.shadowColor = "hsl("+polyHue+" 100 25)";
         chart.fillStyle = "hsl("+polyHue+" 100 50 / 50%)";
         chart.strokeStyle = "hsl("+polyHue+" 100 50)";
         chart.fill();
-        chart.stroke();}; }
+        chart.stroke();
+        for (extra of extras) {
+            chart.beginPath();
+            chart.moveTo((size*.5), (size*.5 + extra[3]*size*.06375));
+            chart.lineTo((size*.5 - extra[4]*size*0.055209118), (size*.5 + extra[4]*size*0.031875));
+            chart.lineTo((size*.5 - extra[5]*size*0.055209118), (size*.5 - extra[5]*size*0.031875));
+            chart.lineTo((size*.5), (size*.5 - extra[0]*size*.06375))
+            chart.lineTo((size*.5 + extra[1]*size*0.055209118), (size*.5 - extra[1]*size*0.031875))
+            chart.lineTo((size*.5 + extra[2]*size*0.055209118), (size*.5 + extra[2]*size*0.031875))
+            chart.closePath();
+            chart.shadowColor = "hsl("+extra[6]+" 100 25)";
+            chart.fillStyle = "hsl("+extra[6]+" 100 50 / 50%)";
+            chart.strokeStyle = "hsl("+extra[6]+" 100 50)";
+            chart.fill();
+            chart.stroke(); }; }; };
 
 document.onkeydown = (e) => {
     if (e.shiftKey == 0) {polyHue += 3 * ((e.key == "ArrowDown") - (e.key == "ArrowUp"));} else {hexSat = Math.min(100, Math.max(0, hexSat + 3 * ((e.key == "ArrowUp") - (e.key == "ArrowDown"))));}
@@ -63,7 +86,15 @@ function mouseDetect(event) {
     var mouseD = 1 + Math.round(.016667*Math.abs(90+ ((180/Math.PI) * Math.atan((event.offsetY-size*.5)/(event.offsetX-size*.5)) + 180*(event.offsetX < size*.5)))) % 6;
     if (mouseC == 0) {if (mouseS != 0) {mouseS = 0}} else {if (mouseS == 0) {mouseS = mouseD}};
     if (mouseS != 0) {stats[mouseS - 1] = mouseV};
-    drawStats(); }
+    drawStats(); };
 drawStats();
 
-div.onmousemove = function(event) {mouseDetect(event)};
+contain.onmousemove = function(event) {mouseDetect(event)};
+add.onmousedown = function(event) {
+    extras.push([stats[0], stats[1], stats[2], stats[3], stats[4], stats[5], polyHue]); 
+    stats = [3, 3, 3, 3, 3, 3];
+    polyHue = 180;
+    console.log(extras);
+    drawStats(); };
+dump.onmousedown = function(event) {extras = []; drawStats();};
+sCompC.onmousedown = function() {sComp = !sComp; drawStats();};
